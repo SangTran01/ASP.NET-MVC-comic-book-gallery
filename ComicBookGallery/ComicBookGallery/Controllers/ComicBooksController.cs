d8b7c87cfe9b4b6d2274dc38f3cde87b6c70138e
sang <stran8089@gmail.com>
2018-07-07 20:07:41 -0400

    created index.cshtml

--- ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGallery.Models;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,23 +10,25 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
         {
+            _comicBookRepository = new ComicBookRepository();
+        }
 
-            var comicBook = new ComicBook();
-            comicBook.SeriesTitle = "The Amazing Spider-Man";
-            comicBook.IssueNumber = 700;
-            comicBook.DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            comicBook.Artists = new Artist[]
-            {
-                new Artist("Script", "Dan Slott"),
-                new Artist("Pencils","Humberto Ramos"),
-                new Artist("Inks","Victor Olazaba"),
-                new Artist("Colors", "Edgar Delgado"),
-                new Artist("Letters","Chris Eliopoulos")
-            };
-            comicBook.Favorite = false;
+        public ActionResult Index() {
+            var comicBooks = _comicBookRepository.GetComicBooks();
+            return View(comicBooks);
+        }
 
+        public ActionResult Detail(int? id)
+        {
+            if (id == null) {
+                return HttpNotFound();
+            }
+
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
             return View(comicBook);
         }
     }
--- ComicBookGallery/ComicBookGallery/Models/Artist.cs
+++ ComicBookGallery/ComicBookGallery/Models/Artist.cs
@@ -10,5 +10,8 @@
             Role = role;
             Name = name;
         }
+
+        public Artist()
+        { }
     }
 }
